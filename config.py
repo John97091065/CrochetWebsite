6133baef6a2134e6e2e76fdd143060f69f8a6388
John97091065 <97091065@st.deltion.nl>
2025-04-11 10:49:38 +0200

    quick add

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+class Config:
+    SECRET_KEY = 'lKDT;<Y[Q&)esFFSv-|xDHxVFg"keKZ:K[.zx8-7H`&gd>aQAcQ{_2JNVZ.L4k('  # For form validation & CSRF protection
+    DB_HOST = 'localhost'
+    DB_USER = 'root'
+    DB_PASSWORD = ''
+    DB_NAME = 'mbs'
