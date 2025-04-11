55e99d3246816a8a673e1eb6205b0b7493128da0
John97091065 <97091065@st.deltion.nl>
2025-04-11 10:14:26 +0200

    none

--- app.py
+++ app.py
@@ -20,10 +20,6 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# Route for registration page
-
-
-
 
 @app.route('/')
 def home():
